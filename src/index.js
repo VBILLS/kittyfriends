943d70d17466bda762d8cf23f1fae77069f38399
Veronica Bills <vbills123@gmail.com>
2019-12-19 15:29:48 -0500

    adding gh-pages for deployment

--- src/index.js
+++ src/index.js
@@ -1,9 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
+
 import App from './containers/App';
-import registerServiceWorker from './registerServiceWorker';
+
+import { searchRobots } from './redux/reducers';
+
+import './index.css';
 import 'tachyons';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import registerServiceWorker from './registerServiceWorker';
+
+const store = createStore(searchRobots);
+
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById('root')
+);
 registerServiceWorker();
